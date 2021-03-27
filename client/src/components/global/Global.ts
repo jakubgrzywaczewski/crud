eec3c3c6cc6c90341c30b2924ab906720add1810
jakubgrzywaczewski <grzywaczewski.jakub@gmail.com>
2021-03-27 21:45:17 +0100

    client: fix font issue

--- client/src/components/global/Global.ts
+++ client/src/components/global/Global.ts
@@ -1,15 +1,22 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+const fontStyle = `
   @import url('https://fonts.googleapis.com/css2?family=Syne+Mono&display=swap');
+`;
 
-    * {
-      font-family: 'Syne Mono', monospace;
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-      font-size: 16px;
-    }
+const head = document.getElementsByTagName('head')[0];
+const style = document.createElement('style');
+style.innerHTML = fontStyle;
+head.appendChild(style);
+
+const GlobalStyle = createGlobalStyle`
+  * {    
+    font-family: 'Syne Mono', monospace;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+  }
 `;
 
 export default GlobalStyle;
